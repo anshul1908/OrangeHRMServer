18eb17064e3526aa1b529d21d1931c52f5daa7c1
ajain <ajain@NODSWL0219.corbus.com>
2018-02-13 14:29:12 +0530

    first file

--- OrangeHRM/src/DefaultPackage/MyClass.java
+++ OrangeHRM/src/DefaultPackage/MyClass.java
@@ -19,6 +19,8 @@ public class MyClass {
 		driver.findElement(By.partialLinkText("click here")).click();
 		System.out.println(driver.getTitle());
 		driver.quit();
+		driver.quit();
+		driver.quit();
 		
 	}
 
